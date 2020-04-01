9542e4f17d30692c3c20adde327c7be43037c573
NoiR_CCC <33998067+NoiRC256@users.noreply.github.com>
2020-04-01 11:28:49 +1100

    Add files via upload

---
+++ VRRig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=tBYl-aSxUe0
+// Sauce: Valem
+
+// Define VR mapping
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map()
+    {
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+// Define Rig
+public class VRRig : MonoBehaviour
+{
+    public float turnSmoothness = 1;
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+    public Transform headConstraint;
+    public Vector3 headBodyOffset;
+
+    // Initialize head transform rules
+    void Start()
+    {
+        headBodyOffset = transform.position - headConstraint.position;
+    }
+
+    // Synchronize VR controls and rig
+    void FixedUpdate()
+    {
+        transform.position = headConstraint.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
+
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
